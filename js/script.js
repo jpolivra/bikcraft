53ece6d3381b5f269eb85e5c33fb4c9a4ab37639
thxjao <jpso.dev@gmail.com>
2021-02-27 18:53:28 -0300

    Added PHP Mailer to form

--- js/script.js
+++ js/script.js
@@ -9,4 +9,12 @@ new SimpleSlide({
 if (window.SimpleAnime) {
 	new SimpleAnime();
 }
+if (window.SimpleForm){
+new SimpleForm({
+ form: ".formphp", // Form Selector
+ button: "#enviar", // Button Selector
+ erro: "<div id='form-erro'><h2>Erro no envio :(</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>",
+ sucesso: "<div id='form-sucesso'> <h2>Formulário enviado com sucesso. :)</h2><p>Em breve responderemos seu email!</p></div>"
+});
+}
 
